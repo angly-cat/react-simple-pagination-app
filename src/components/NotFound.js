5068d363ad1cd8d637bb5a7af334f39cb972b9dc
Angly Cat <angly.cat@gmail.com>
2017-12-07 05:31:08 +0600

    Prepare app for GH-pages

--- src/components/NotFound.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (<h1 className='text-center'>404 Not Found</h1>);
-
-export default NotFound;
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const LOCAL_STORAGE_USER_KEY = 'user';
-export const LOCAL_STORAGE_BASE_URL_KEY = 'baseUrl';
-export const EDIT_TOKEN = 'beejee';
